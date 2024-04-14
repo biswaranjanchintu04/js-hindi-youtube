2680d3508ddbd2a95d3355ac292fd69d13e69ba7
biswaranjanchintu04 <161107276+biswaranjanchintu04@users.noreply.github.com>
2024-04-14 07:01:52 +0000

    datatype

---
+++ 01.basic/02_datatype.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all js code as new version
+
+// alert(3+3) // we are using node js
+
+// console.log(0+0) // code readability should be high
+
+console.log("chintu")
+let name = "chintu"
+let age = 12
+let isLoggedIn = false
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standlone value
+// let state ;     undefined 
+// symbol => uniqe 
+
+// object 
+console.log(typeof "hitesh");
+console.log(typeof age);
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
